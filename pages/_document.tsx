924e53b0dd8cde90c8ae220a426f22cbc98483fe
Vladislav Manolov <vmanolov2000@gmail.com>
2026-04-14 10:21:17 +0200

    Customized the HTML structure & added metadata

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        <title>Business Idea Generator</title>
+        <meta name="description" content="AI-powered business idea generation" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
