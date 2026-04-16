fa473e69f523ff6fe5e2bce53459074f819de35d
Vladislav Manolov <vmanolov2000@gmail.com>
2026-04-16 09:52:05 +0200

    Updated App Config & Added Consult Form

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ClerkProvider } from '@clerk/nextjs';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import 'react-datepicker/dist/react-datepicker.css';
 
 // Wrapping the entire application with ClerkProvider to enable authentication features across all pages.
 export default function MyApp({ Component, pageProps }: AppProps) {
